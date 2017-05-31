8a31a868ca9dd85c2b307201b89c75a7a7769cda
san <skysan@gmail.com>
2017-05-31 22:51:44 +0530

    removed pythonReact app from container

--- app/js/HelloWorld.jsx
+++
@@ -1,9 +0,0 @@
-export class HelloWorld extends React.Component {
-  render() {
-    return (
-      <div className="helloworld">
-        Isomorphic Hello {this.props.name}
-      </div>
-    );
-  }
-}
